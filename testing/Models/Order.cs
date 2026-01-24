8f62245222bed0c803caa11ae2f256c293a2ab91
yassinou19 <chahid1yassine@gmail.com>
2026-01-24 18:49:23 +0100

    remove

--- testing/Models/Order.cs
+++ testing/Models/Order.cs
@@ -13,7 +13,6 @@ public class Product
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public string Name1 { get; set; }
     public decimal Price { get; set; }
     public int StockQuantity { get; set; }
 }
