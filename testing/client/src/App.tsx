56c1014b54b9e381811e45dafd6f1fd0eae3b265
yassinou19 <chahid1yassine@gmail.com>
2026-01-31 21:49:13 +0100

    g

--- testing/client/src/App.tsx
+++ testing/client/src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Bienvenue sur mon application FRONT</h1>
-      <p>Ceci est une page statique simple générée avec Vite et FRONT.</p>
+      <p>Ceci est une page statique simple générée avec Vite et React.</p>
     </div>
   )
 }
