ba188a195e17054f056926a3c3f8ad8a65b9b5f9
yassinou19 <chahid1yassine@gmail.com>
2026-01-31 21:57:09 +0100

    WEBHOOK

--- testing/client/src/App.tsx
+++ testing/client/src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Bienvenue sur mon application FRONT</h1>
-      <p>Ceci est une page statique simple générée avec Vite et React.</p>
+      <h1>Bienvenue sur mon application WEBHOOK</h1>
+          <p>Ceci est une page statique simple générée avec Vite et WEBHOOK.</p>
     </div>
   )
 }
