5a4dc003117895eb394c0b76e10724cac49f9634
yassinou19 <chahid1yassine@gmail.com>
2026-01-31 19:48:52 +0100

    add front

---
+++ testing/client/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Bienvenue sur mon application React</h1>
+      <p>Ceci est une page statique simple générée avec Vite et React.</p>
+    </div>
+  )
+}
+
+export default App
